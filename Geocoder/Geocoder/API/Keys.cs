f81f91a33894f2e68fa39db4167a7c89fd3c3ed6
Blaine Harris <blaine91787@gmail.com>
2019-02-06 22:43:10 -0600

    First commit. Added the Geocoding library and GeocodingTester

---
+++ Geocoder/Geocoder/API/Keys.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Geocoding.API
+{
+    public static class Keys
+    {
+        public static string KeysXml = @"PATH_TO_KEYS.XML";
+
+        public static string GoogleGeocoding
+        {
+            get
+            {
+                try
+                {
+                    XmlDocument xdoc = new XmlDocument();
+
+                    if (!File.Exists(KeysXml))
+                    {
+                        throw new FileNotFoundException("Keys.xml could not be found");
+                    }
+
+                    xdoc.Load(KeysXml);
+                    string apiKey = xdoc.GetElementsByTagName("GoogleGeocoding").Item(0).InnerText;
+                    return apiKey;
+                }
+                catch (FileNotFoundException e)
+                {
+                    Console.WriteLine("Make sure the Keys.xml file exists, and you've provided the path.");
+                    throw e;
+                }
+            }
+        }
+    }
+}
---
+++ Geocoder/GeocoderTester/Program.cs
@@ -0,0 +1,52 @@
+using Geocoding;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeocoderTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // BEFORE YOU BEGIN: include your Google Geocoding API key in API/keys.xml and update the xml path in Keys.cs
+
+            try
+            {
+                Geocoder location = new Geocoder(streetNumber: "1450", streetName: "Jayhawk Blvd", city: "Lawrence", state: "KS", postalCode: "66045");
+                Console.WriteLine("Formatted Address: {0}", location.FormattedAddress);
+                Console.WriteLine("Street Number: {0}", location.StreetNumber);
+                Console.WriteLine("Street Name: {0}", location.StreetName);
+                Console.WriteLine("City: {0}", location.City);
+                Console.WriteLine("State: {0}", location.State);
+                Console.WriteLine("State Abbreviation: {0}", location.StateAbbreviation);
+                Console.WriteLine("Postal Code: {0}", location.PostalCode);
+                Console.WriteLine("Postal Code Suffix: {0}", location.PostalCodeSuffix);
+                Console.WriteLine("County: {0}", location.County);
+                Console.WriteLine("Country: {0}", location.Country);
+                Console.WriteLine("Country Abbreviation: {0}", location.CountryAbbreviation);
+                Console.WriteLine("Latitude: {0}", location.Latitude);
+                Console.WriteLine("Longitude: {0}", location.Longitude);
+                Console.WriteLine("Place ID: {0}", location.PlaceID);
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine("Error: {0}", e.Message);
+                Console.WriteLine("Verify you've provided a valid path to the file Keys.xml");
+            }
+            catch (InvalidOperationException e)
+            {
+                Console.WriteLine("Error: {0}", e.Message);
+                Console.WriteLine("Verify you've provided a valid Google Geocoding API key in Keys.xml");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: {0}", e.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
